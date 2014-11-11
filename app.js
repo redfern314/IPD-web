cfe36f252d3b4bc7763925c13de011b38b283244
Derek Redfern <redfern.derek@gmail.com>
2014-11-11 10:38:05 -0500

    Mongoose installed

--- app.js
+++ app.js
@@ -8,6 +8,32 @@
 var dotenv = require('dotenv');
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/ipd");
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    console.log("Open!");
+    var lovedOneSchema = mongoose.Schema({
+        lat: String,        // latitude of loved one
+        lon: String,        // longitude of loved one
+        address: String,    // address of loved one
+        updated: Date       // date/time this item was last updated
+    });
+    var lovedOneModel = mongoose.model('Loved One Data',lovedOneSchema);
+    var lovedOneData = new lovedOneModel({updated:Date.now});
+
+    var caregiverSchema = mongoose.Schema({
+        lat: String,        // latitude of caregiver
+        lon: String,        // longitude of caregiver
+        reldir: Number,     // compass direction between 0 and 359
+        updated: Date,      // date/time this item was last updated
+
+    });
+    var caregiverModel = mongoose.model('Caregiver Data',caregiverSchema);
+    var caregiverData = new lovedOneModel({updated:Date.now});
+});
 
 // Load the environment variables from the .env file
 dotenv.load();
@@ -18,8 +44,8 @@ app.use(express.static(__dirname + '/public'));
 app.locals.pretty = true; // prettify HTML
 
 // HTTP endpoints
-
 app.get("/",function(req,res){
+    // render some default text
     res.send("IPD Team 3 - Backend Web Server");
 });
 
